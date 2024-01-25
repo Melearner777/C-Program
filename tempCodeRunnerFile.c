fdfc5c16584487e859a9fc70bbe16718e47b3ade
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-25 19:12:04 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a:");
+    scanf("%d",&a);
+    int b;
+    printf("Enter b:");
+    scanf("%d",&b);
+    int  multiply=a*b;
+    printf("The multiplication of a and b is : %d",multiply);
+    return 0;
+    
+}
