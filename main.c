e051307582aee3fedfd07ca0122f5a99359cbf74
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-25 19:11:19 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the value if n :");
+    scanf("%d",&n);
+    for (int i=0;i<=n;i++){
+        printf(" hello world ");
+    }  
+      
+    printf("\n");
+    return 0;
+    
+}
