fd39152782c884efffd7f1cc99afed5375644e86
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-26 22:01:22 +0530

    Add files via upload

---
+++ Tut.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=41;
+    float b=6;
+
+    printf("a+b=%f\n",a+b);
+    printf("a-b=%f\n",a-b);
+    printf("a/b=%f\n",a/b);
+    printf("a*b=%f\n",a*b);
+
+    return 0;
+
+}
