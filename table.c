69562623ad7055c787b2b72c8af1de19a96a43be
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-26 22:01:08 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the Table:");
+    scanf("%d",&a);
+   
+    int multiply=a*1;
+    printf("Table of input is : %d",multiply);
+    return 0;
+
+}
