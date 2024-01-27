081efe8a74de9a52c81c163003db105baf4e570a
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-27 14:07:55 +0530

    Add files via upload

---
+++ escapeseq.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+
+    int a=5;
+    float b =7.83;
+    printf("Value of a is :%d\n",a);
+    printf("Value of b is: %7.4f\n",b);
+    printf("%-18.4f this",b);
+    return 0;
+}
