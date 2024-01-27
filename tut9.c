94f42e964abef24a0adcd27823013129fba80c8e
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-27 14:08:03 +0530

    Add files via upload

---
+++ tut9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    float b=3.48;
+    printf("print the value of a:%d\n",a);
+    printf("print the float value of b: %f",b);
+    printf("We can print both line in same line\n");
+    printf("Value of a is %d\nValue of b is %f",a,b);
+    return 0;
+}
