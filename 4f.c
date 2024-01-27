b00eb9ba7ba3262e58b5f11e505b7afb87a7b773
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-27 14:07:29 +0530

    Add files via upload

---
+++ 4f.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=5.6;
+    float b=7.148;
+    printf("Hello world");
+    printf("Value of a is %d\n value of b is %.4f\n",a,b);
+    //value after point is use to print the number of digit we want 
+    //just as .4 use as to print the digit of 4 
+    return 0;
+
+}
