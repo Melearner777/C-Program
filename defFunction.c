605f42820a89e7be7324193cb8d6d09e7c7854e9
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-27 14:07:42 +0530

    Add files via upload

---
+++ defFunction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+    int a=786;
+    const float b=3.15;
+    printf("Value of a is :%d\n",a);
+    printf("Value of b is :%.2f\n",b);
+
+    printf("Value of pi is:%.2f",PI);
+    return 0;
+
+}
+
