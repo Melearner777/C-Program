b1e67cf22cc08f87d7ef61c41d8b6c04caf3326e
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-28 18:35:24 +0530

    Add files via upload

---
+++ try2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a:");
+    scanf("%d",&a);
+    int b;
+    printf("Enter b:");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("The sum of %d and %d is %d",sum);
+    return 0;
+    
+}
