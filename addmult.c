1d3ba457d9d6cd8ecbc9bc777cbba2293de7306e
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-31 19:49:56 +0530

    Add files via upload

---
+++ addmult.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int a,b,c,d;
+    printf("Enter the number a:");
+    scanf("%d",&a);
+    printf("Enter the number b:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Sum of both number:%d\n",c);
+    d=a*b;
+    printf("Multiple of both number:%d",d);
+
+    return 0;
+
+
+}
