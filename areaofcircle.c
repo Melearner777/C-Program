5994f14d2d49f4a36697f32b4ca9045d29b272cb
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-31 19:49:45 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int r;
+    float result;
+    printf("Enter the radius:");
+    scanf("%d",&r);
+    result=3.14*r*r;
+    printf("Area of circle is %.2f unit_square\n",result);
+    
+
+    return 0;
+
+}
