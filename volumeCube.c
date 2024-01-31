0f3979aeb9e532dcb5fd03ef42057e4f7afc3378
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-01-31 19:49:15 +0530

    Add files via upload

---
+++ volumeCube.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int h,w,d,V;
+    printf("Enter the value of h:");
+    scanf("%d",&h);
+    printf("Enter the value of w:");
+    scanf("%d",&w);
+    printf("Enter the value of d:");
+    scanf("%d",&d);
+    V=h*w*d;
+    printf("Volume of cubiod is:%d",V);
+
+    return 0;
+}
