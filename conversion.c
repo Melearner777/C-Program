a139c2f82b71f7d60ecdd29dcb5a5105bd95248b
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-05 22:35:24 +0530

    Add files via upload

---
+++ conversion.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int k, m;
+    printf("Enter the value in Kilometer:");
+    scanf("%d",&k);
+    m=k*1000;
+    printf("The value is: %d m",m);
+    return 0;
+}
